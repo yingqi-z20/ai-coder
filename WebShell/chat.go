4d8f58eb3c45bb477a4581273eee44f4605fc59a
yingqi-z20 <z_1501609820@163.com>
2026-04-19 23:58:36 +0800

    prid

--- WebShell/chat.go
+++ WebShell/chat.go
@@ -258,6 +258,8 @@ func Qwen(c *gin.Context) {
 		case <-cx.Done():
 			break
 		}
+		eid = stream.Current().Response.ID
+		prid.Store(&eid)
 		stream = client.Responses.NewStreaming(ctx, responses.ResponseNewParams{
 			Model:              "qwen3.6-plus",
 			PreviousResponseID: openai.String(*prid.Load()),
